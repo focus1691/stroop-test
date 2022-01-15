9d330358e32b85e7f9953defb114c10224a67148
Joshua <mrjoshuaali@yahoo.co.uk>
2022-01-15 20:34:59 +0000

    video fix

--- src/scenes/introductionScene.js
+++ src/scenes/introductionScene.js
@@ -8,15 +8,20 @@ export default class introductionScene extends Phaser.Scene {
     this.load.video('psychotechnology', NODE_ENV === 'development' ? psychotechnologyVideo : 'https://www.psychotechnology.com/assets/videos/welcome.mp4', 'loadeddata', false, false);
   }
   create() {
-    var vid = this.add.video(this.game.config.width / 2, this.game.config.height / 2, 'psychotechnology');
-    vid.setDisplaySize(this.game.config.width, this.game.config.height);
-
-    vid.play(false);
-
-    // Prevents video freeze when game is out of focus (i.e. user changes tab on the browser)
-    vid.setPaused(false);
-
-    vid.on('complete', this.onComplete, this);
+    try {
+      var vid = this.add.video(this.game.config.width / 2, this.game.config.height / 2, 'psychotechnology');
+      vid.setMute(true);
+      vid.setDisplaySize(this.game.config.width, this.game.config.height);
+  
+      vid.play(false);
+  
+      // Prevents video freeze when game is out of focus (i.e. user changes tab on the browser)
+      vid.setPaused(false);
+      vid.on('complete', this.onComplete, this);
+    } catch (error) {
+      console.log(error);
+      this.onComplete();
+    }
   }
   onComplete() {
     this.scene.start('preloaderScene');
